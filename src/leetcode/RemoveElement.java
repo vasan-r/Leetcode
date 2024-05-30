c93dd0ba55e6acdf2b0a4a64a7ef4ad38a19f955
vasan-r <aavasan2002@gmail.com>
2024-05-30 23:59:15 +0530

    Remove element

---
+++ src/leetcode/RemoveElement.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,2,3,0,4,2};
+        System.out.println(removeElement(nums,2));
+        System.out.println(Arrays.toString(nums));
+    }
+    public static int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != val){
+                nums[count++] = nums[i];
+            }
+        }
+        return count;
+    }
+}
