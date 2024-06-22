79dfa0aa9afbb71b4868880622a122c6d9c1f936
vasan-r <aavasan2002@gmail.com>
2024-06-22 23:56:58 +0530

    leetcode problems

---
+++ src/leetcode/StringReverse.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        String s = "vasan is good boy";
+
+//        String[] temp = s.split(" ");
+//        System.out.println(Arrays.toString(temp));
+//        for (int i = temp.length-1; i >= 0; i--) {
+//            System.out.print(temp[i]+" ");
+//        }
+//        String[] temp = s.split(" ");
+//        String str = "";
+//        System.out.println(Arrays.toString(temp));
+//        for (int i = 0; i < temp.length; i++) {
+//            String t = temp[i];
+//            String con = "";
+//            for (int j = t.length()-1; j >= 0; j--) {
+//                con += t.charAt(j);
+//            }
+//            System.out.println(con);
+//            str += con+" ";
+//        }
+//        System.out.println(str.trim());
+        int num = 8;
+        System.out.println(num<<1);
+    }
+}
